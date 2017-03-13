11f56b3f07ac8f61db86df63b1f1da1e090feaac
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-13 11:32:57 +0100

    initial set up test: it seats requested match theater size it suggests all seats

--- src/theater/core.clj
+++ src/theater/core.clj
@@ -1,1 +1,15 @@
 (ns theater.core)
+
+; a theater is represented as a list of rows represented by letters
+; (the lower the letter, the closest to the front)
+; with the available seats marked with 0,
+; and the occupied seats with 1
+; e.g. this represents a 5x5 theater with all seats available
+; [{:A [0,0,0,0,0]},
+;  {:B [0,0,0,0,0]},
+;  {:C [0,0,0,0,0]},
+;  {:D [0,0,0,0,0]},
+;  {:E [0,0,0,0,0]}]
+
+(defn suggest [theater party-size]
+  [{:A 1} {:A 2}])
--- test/theater/core_test.clj
+++ test/theater/core_test.clj
@@ -1,3 +1,8 @@
 (ns theater.core-test
   (:use midje.sweet)
   (:use [theater.core]))
+
+(facts "about suggesting seats"
+       (fact "on an two seats empty teather it suggests these seats"
+             (let [theater [{:A [0 0]}]]
+               (suggest theater 2)) => [{:A 1} {:A 2}]))
