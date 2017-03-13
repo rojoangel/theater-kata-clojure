384609b06f66bb15cf8d1bb0fbc5bee12981a0cb
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-03-13 13:00:44 +0100

    add row->seats with some auxiliary methods

--- src/theater/core.clj
+++ src/theater/core.clj
@@ -5,13 +5,26 @@
 ; with the available seats marked with :free,
 ; and the occupied seats with :occupied
 ; e.g. this represents a 5x5 theater with all seats available
-; [{:A [:free :free :free :free :free]},
-;  {:B [:free :free :free :free :free]},
-;  {:C [:free :free :free :free :free]},
-;  {:D [:free :free :free :free :free]},
-;  {:E [:free :free :free :free :free]}]
+; [[:A [:free :free :free :free :free]],
+;  [:B [:free :free :free :free :free]],
+;  [:C [:free :free :free :free :free]],
+;  [:D [:free :free :free :free :free]],
+;  [:E [:free :free :free :free :free]]]
+
+(defn- row->letter [row]
+  (first row))
+
+(defn- row->seat-availability [row]
+  (second row))
+
+(defn- row->size [row]
+  (count (row->seat-availability row)))
+
+(defn- row->seats [row]
+  (let [row-size (row->size row)
+        seat-nums (range 1 (inc row-size))
+        seat-letters (repeat row-size (row->letter row))]
+    (map vector seat-letters seat-nums)))
 
 (defn suggest [theater party-size]
-  (if (= party-size 2)
-    [{:A 1} {:A 2}]
-    [{:A 1} {:A 2} {:A 3}]))
+  (row->seats (first theater)))
--- test/theater/core_test.clj
+++ test/theater/core_test.clj
@@ -4,7 +4,9 @@
 
 (facts "about suggesting seats"
        (fact "on an empty teather when party size equals theater size suggests all seats"
-             (let [theater [{:A [:free :free]}]]
-               (suggest theater 2)) => [{:A 1} {:A 2}]
-             (let [theater [{:A [:free :free :free]}]]
-               (suggest theater 3)) => [{:A 1} {:A 2} {:A 3}]))
+             (let [theater [[:A [:free :free]]]]
+               (suggest theater 2)) => [[:A 1] [:A 2]]
+             (let [theater [[:A [:free :free :free]]]]
+               (suggest theater 3)) => [[:A 1] [:A 2] [:A 3]]
+             (let [theater [[:A [:free :free :free :free]]]]
+               (suggest theater 3)) => [[:A 1] [:A 2] [:A 3] [:A 4]]))
